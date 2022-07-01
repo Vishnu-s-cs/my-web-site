4c819c2521d527239b21953d8aed89e6a2798545
Vishnu <vishnuvichuz198@gmail.com>
2022-07-01 15:39:50 +0530

    form validation done

--- custom.js
+++ custom.js
@@ -1,16 +1,27 @@
-$("#submit-form").validate({
-   rules:{
-        fname:{
-            minlength: 2
-        }
-   },
-   messages: {
-    fname:{
-        required: "please enter your first name",
-        minlength: "should contain atleast 2 charcters"
+jQuery('#submit-form').validate({
+  
+    rules:{
+        firstname:"required",
+        lastname:"required",
+        email:"required",
+        number:{
+            required:true,
+            minlength:10,
+            maxlength:10
+        },
+        subject:"required"
+    },
+    messages:{
+        firstname:"Please Enter your name",
+        lastname:"Please Enter Your Last Name",
+        email:"Enter your valid email  id",
+        number:"Enter Valid mobile no.",
+        subject:"Just give me a feedback",
+       
+    },
+    submitHandler:function(form) {
+        form.submit();
     }
-  },
-  submitHandler: function(form) {
-      form.submit();
-    }
-   });
+  
+
+})
